7671337ca7f0ace4476d4f099dc7d2aa22912073
S@BBIR <87950047+KNI9H7-SH4D0W@users.noreply.github.com>
2022-06-11 00:11:50 +0600

    Update hammer.py

--- hammer.py
+++ hammer.py
@@ -6,7 +6,7 @@
 
 from queue import Queue
 from optparse import OptionParser
-import time,sys,socket,threading,logging,urllib.request,random
+import time,sys,socket,threading,logging,urllib.request,random,os
 
 def user_agent():
 	global uagent
